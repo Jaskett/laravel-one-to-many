0e8161f8014682bfc7a10d8540261d37027bba91
Brandon Vasquez <thatsjaskett@gmail.com>
2023-03-27 10:33:26 +0200

    Creato Seeder Type

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -5,6 +5,12 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+// Models
+use App\Models\Type;
+
+// Helpers
+use Illuminate\Support\Str;
+
 class TypeSeeder extends Seeder
 {
     /**
@@ -14,6 +20,19 @@ class TypeSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $types = [
+            'Blog',
+            'Portfolio',
+            'Information',
+            'Event',
+            'Forum'
+        ];
+
+        foreach ($types as $type) {
+            $newType = Type::create([
+                'name' => $type,
+                'slug' => Str::slug($type)
+            ]);
+        }
     }
 }
